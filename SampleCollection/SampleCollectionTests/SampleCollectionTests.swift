d631ec0d8ebcf9aad53dc2a4110ec91f5bb974e2
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-07-15 15:46:04 +0900

    build: 초기 프로젝트 파일 추가

---
+++ SampleCollection/SampleCollectionTests/SampleCollectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCollectionTests.swift
+//  SampleCollectionTests
+//
+//  Created by 이재훈 on 7/15/25.
+//
+
+import Testing
+@testable import SampleCollection
+
+struct SampleCollectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
